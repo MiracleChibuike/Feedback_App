4dc21fc7c7d2dd24fb98ef117a0ca986d61568fc
MiracleChibuike <miraclechibuike27@gmail.com>
2024-06-13 13:23:37 +0100

    Updated the training feedback form - completed

--- src/components/feedback-Forms/Last_Feedback.jsx
+++ src/components/feedback-Forms/Last_Feedback.jsx
@@ -17,30 +17,83 @@ const Last_Feedback = () => {
     const Landing_nav = () => {
         navigate_landing("/LandingPage");
     }
+
+     const modalToggle = () => {
+       const modalStrong = document.getElementById("modal_strong");
+       modalStrong.style.display = "none";
+     };
+
+    // VALIDATE THE TRAINING FEEDBSCK FORM
+
+    const validate_Training_Feedback = (e) => {
+      e.preventDefault();
+       const modalStrong = document.getElementById("modal_strong");
+
+       let training, location, trainers, date_Of_Training, additional_Comments
+
+       training = document.getElementById("Form-Title");
+       location = document.getElementById("form-locate");
+       trainers = document.getElementById("form_trainer");
+       date_Of_Training = document.getElementById("training_date");
+       additional_Comments = document.getElementById("Comments_addition");
+
+        const display_Data = `Your Response was saved as follows \n
+        Title of Training: ${training.value} \n
+        Location of Training: ${location.value}  \n
+        Trainers: ${trainers.value}  \n
+        Date of Training: ${date_Of_Training.value};  \n
+        Additional Comments: ${additional_Comments.value}  \n
+        `;
+        console.log(display_Data)
+         modalStrong.style.display = "block";
+         alert(display_Data)
+
+         const stringiFication = JSON.stringify(display_Data);
+        localStorage.setItem("data_Users", stringiFication);
+        var data_Users = localStorage.getItem("data_Users");
+        var check = JSON.parse(data_Users);
+
+        
+        console.log(data_Users)
+
+        document.querySelector("form").reset();
+
+       const Object_Data = {
+         Title: `${training.value}`,
+         Location: `${location.value}`,
+         Trainers: `${trainers.value} `,
+         Date: `${date_Of_Training.value}`,
+       };
+
+       console.log(Object_Data)
+
+
+    }
  
     return (
       <div className="container">
-        <div className="form_Body">
-          <div className="canCel">
-            <img
-              src={CancelIcon}
-              id="cancel_icon"
-              onClick={Landing_nav}
-              alt="canceL_Dev"
-            />
-          </div>
-          <div className="heading">
-            <h2>TRAINING FEEDBACK FORM</h2>
-          </div>
-          <div className="formHead">
-            <form action="">
+        <form onSubmit={validate_Training_Feedback} autoComplete="on">
+          <div className="form_Body">
+            <div className="canCel">
+              <img
+                src={CancelIcon}
+                id="cancel_icon"
+                onClick={Landing_nav}
+                alt="canceL_Dev"
+              />
+            </div>
+            <div className="heading">
+              <h2>TRAINING FEEDBACK FORM</h2>
+            </div>
+            <div className="formHead">
+              {/* <form action=""> */}
               <div className="form_Title">
                 <label htmlFor="Form-Title" className="labelIn">
                   TITLE OF TRAINING{" "}
                 </label>{" "}
                 <br />
                 <input
-                  type="email"
+                  type="text"
                   placeholder="Ex. Web Development"
                   required
                   id="Form-Title"
@@ -66,73 +119,123 @@ const Last_Feedback = () => {
               </div>
               <div className="date">
                 <label htmlFor="form_date">DATE</label> <br />
-                <input type="date" placeholder="d/m/y" />
+                <input type="date" placeholder="d/m/y" id="training_date" required />
               </div>
-            </form>
+              {/* </form> */}
+            </div>
           </div>
-        </div>
-        <div className="Experience_rate">
-          <p>
-            Please Provide a rating for each of the statements below, by
-            checking in the corresponding box
-          </p>
-          <div className="First_rating">
-            <p>The training objectives were clearly defined at the beginning</p>
-            <div className="check_rate">
-              <div className="S_Agree">
-                <input type="checkbox" id="S_Agree" /> <br />
-                <label htmlFor="S_Agree">Strongly Agree</label>
+          <div className="Experience_rate">
+            <p>
+              Please Provide a rating for each of the statements below, by
+              checking in the corresponding box
+            </p>
+            <div className="First_rating">
+              <p>
+                The training objectives were clearly defined at the beginning
+              </p>
+              <div className="check_rate">
+                <div className="S_Agree">
+                  <input type="checkbox" id="S_Agree" /> <br />
+                  <label htmlFor="S_Agree">Strongly Agree</label>
+                </div>
+                {/* Agree */}
+                <div className="Agree">
+                  <input type="checkbox" id="Agree" /> <br />
+                  <label htmlFor="Agree">Agree</label>
+                </div>
+                {/* Neutral */}
+                <div className="Neutral">
+                  <input type="checkbox" id="Neutral" /> <br />
+                  <label htmlFor="Neutral">Neutral</label>
+                </div>
+                {/* Disagree */}
+                <div className="Disagree">
+                  <input type="checkbox" id="Disagree" /> <br />
+                  <label htmlFor="Disagree">Disagree</label>
+                </div>
+                {/* Strongly_Disagree */}
+                <div className="S_Disagree">
+                  <input type="checkbox" id="S_Disagree" /> <br />
+                  <label htmlFor="S_Disagree">Strongly Disagree</label>
+                </div>
               </div>
-              {/* Agree */}
-              <div className="Agree">
-                <input type="checkbox" id="Agree" /> <br />
-                <label htmlFor="Agree">Agree</label>
-              </div>
-              {/* Neutral */}
-              <div className="Neutral">
-                <input type="checkbox" id="Neutral" /> <br />
-                <label htmlFor="Neutral">Neutral</label>
-              </div>
-              {/* Disagree */}
-              <div className="Disagree">
-                <input type="checkbox" id="Disagree" /> <br />
-                <label htmlFor="Disagree">Disagree</label>
-              </div>
-              {/* Strongly_Disagree */}
-              <div className="S_Disagree">
-                <input type="checkbox" id="S_Disagree" /> <br />
-                <label htmlFor="S_Disagree">Strongly Disagree</label>
+            </div>
+            {/* EXP 2 */}
+            <div className="First_rating">
+              <p>Participation was encouraged throughout the training.</p>
+              <div className="check_rate">
+                <div className="S_Agree_2">
+                  <input type="checkbox" id="S_Agree_2" /> <br />
+                  <label htmlFor="S_Agree_2">Strongly Agree</label>
+                </div>
+                {/* Agree */}
+                <div className="Agree_2">
+                  <input type="checkbox" id="Agree_2" /> <br />
+                  <label htmlFor="Agree_2">Agree</label>
+                </div>
+                {/* Neutral */}
+                <div className="Neutral_2">
+                  <input type="checkbox" id="Neutral_2" /> <br />
+                  <label htmlFor="Neutral_2">Neutral</label>
+                </div>
+                {/* Disagree */}
+                <div className="Disagree_2">
+                  <input type="checkbox" id="Disagree_2" /> <br />
+                  <label htmlFor="Disagree_2">Disagree</label>
+                </div>
+                {/* Strongly_Disagree */}
+                <div className="S_Disagree_2">
+                  <input type="checkbox" id="S_Disagree_2" /> <br />
+                  <label htmlFor="S_Disagree_2">Strongly Disagree</label>
+                </div>
               </div>
             </div>
           </div>
-          {/* EXP 2 */}
-          <div className="First_rating">
-            <p>Participation was encouraged throughout the training.</p>
-            <div className="check_rate">
-              <div className="S_Agree_2">
-                <input type="checkbox" id="S_Agree_2" /> <br />
-                <label htmlFor="S_Agree_2">Strongly Agree</label>
+
+          {/* Additional Comments */}
+          <div className="Addition">
+            <div className="comments">
+              <label htmlFor="Comments_addition">Additional Comments</label>
+              <textarea
+                id="Comments_addition"
+                required
+                placeholder="Please provide additional comments different from the one's stated above regarding your training"></textarea>
+            </div>
+          </div>
+          {/* Submit Button */}
+          <div className="submit">
+            <button type="submit" id="training_submit_button">
+              Submit Your Feedback
+            </button>
+          </div>
+        </form>
+        {/* MODAL TO SHOW A SUCCESSFUL USER SUBMISSION */}
+        <div className="modal" id="modal_strong">
+          <div className="modal_Display">
+            <div className="modalContainer">
+              <div className="cancelDiv">
+                <img
+                  src={CancelIcon}
+                  // id="cancel_icon"
+                  className="removeMode"
+                  onClick={modalToggle}
+                  alt=""
+                />
               </div>
-              {/* Agree */}
-              <div className="Agree_2">
-                <input type="checkbox" id="Agree_2" /> <br />
-                <label htmlFor="Agree_2">Agree</label>
-              </div>
-              {/* Neutral */}
-              <div className="Neutral_2">
-                <input type="checkbox" id="Neutral_2" /> <br />
-                <label htmlFor="Neutral_2">Neutral</label>
-              </div>
-              {/* Disagree */}
-              <div className="Disagree_2">
-                <input type="checkbox" id="Disagree_2" /> <br />
-                <label htmlFor="Disagree_2">Disagree</label>
-              </div>
-              {/* Strongly_Disagree */}
-              <div className="S_Disagree_2">
-                <input type="checkbox" id="S_Disagree_2" /> <br />
-                <label htmlFor="S_Disagree_2">Strongly Disagree</label>
-              </div>
+              <p className="feedback_success_Msg">
+                Feedback submitted succesfully <br />
+                <img
+                  src={success_Icon}
+                  alt="successful feedback indicator"
+                  id="successful"
+                />{" "}
+                <br />
+                Thank You
+              </p>
+              {/* Button Home */}
+              <button id="landing_route_btn" onClick={Landing_nav}>
+                Back to home
+              </button>
             </div>
           </div>
         </div>
